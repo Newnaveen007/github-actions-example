3ee339f709e6c24c9242ae9e1033f1453591fec5
Naveen kumar <hope027is@gmail.com>
2022-09-18 19:12:50 +0500

    code

---
+++ src/test/java/com/web/cicd/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
